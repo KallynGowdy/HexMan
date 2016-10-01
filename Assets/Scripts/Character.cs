58231ee29159563e07763f960f9e61fc1cd1720c
Kallyn Gowdy <kallyngowdy@gmail.com>
2016-10-01 13:38:25 -0400

    Rework main menu grid system

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Gamelogic.Grids;
+using GridPoint2 = Gamelogic.Grids2.GridPoint2;
+
+public class Character : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
