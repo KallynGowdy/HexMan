4d107727a1acd688a6f5cc02c0516f9dc8bf3692
Kallyn Gowdy <kallyngowdy@gmail.com>
2016-10-01 22:05:35 -0400

    Add basic level loading

---
+++ Assets/Scripts/Rules/Actions/LoadLevelAction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Defines an action that is able to load a new game level.
+/// </summary>
+public class LoadLevelAction : RuleAction
+{
+    public string SceneName;
+    public GameObject World;
+    public GameObject LoadingScreen;
+
+    public override void Apply(RuleData data)
+    {
+        DestroyObject(World);
+        Instantiate(LoadingScreen);
+        SceneManager.LoadSceneAsync(SceneName);
+    }
+}
